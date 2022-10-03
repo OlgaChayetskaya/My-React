7bf0106ff4f5199c1a72035548b34cb7df44a127
OlgaChayetskaya <Ochayetskaya@ibagroup.eu>
2022-10-03 12:17:43 +0200

    Task 3

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -2,13 +2,14 @@ import Card from "../UI/Card";
 import "./ProjectItem.css";
 
 function ProjectItem(props) {
+  
   return (
-    <Card className="project-item">
-      <div className="project-item__description">
-        <h2>{props.projName}</h2>
-        <div className="project-year">{props.year}</div>
-        <div className="project-item__customer">{props.customer}</div>
-      </div>
+    <Card className={`project-item ${props.checkBoxState ? 'barbie':''}`}>
+      <div className={`project-item__description ${props.checkBoxState ? 'barbie':''}`}>
+        <h2>{props.projName}</h2>        
+        <div className={`project-year ${props.checkBoxState ? 'barbie':''}`}>{props.year}</div>
+        <div className={`project-item__customer ${props.checkBoxState ? 'barbie':''}`}>{props.customer}</div>
+      </div>      
     </Card>
   );
 }
