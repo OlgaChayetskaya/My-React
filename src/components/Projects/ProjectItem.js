f59c82e979861702036b8b7d08075c52a404a4f4
OlgaChayetskaya <Ochayetskaya@ibagroup.eu>
2022-10-04 11:40:40 +0200

    Task 3 upd

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,15 +1,35 @@
+import React, { useState } from "react";
+
 import Card from "../UI/Card";
+
 import "./ProjectItem.css";
 
 function ProjectItem(props) {
-  
+  const [isChecked, setIsChecked] = useState(false);
+
+  const changeStateHandler = (event) => {
+    setIsChecked(!isChecked);
+    return;
+  };
   return (
-    <Card className={`project-item ${props.checkBoxState ? 'barbie':''}`}>
-      <div className={`project-item__description ${props.checkBoxState ? 'barbie':''}`}>
-        <h2>{props.projName}</h2>        
-        <div className={`project-year ${props.checkBoxState ? 'barbie':''}`}>{props.year}</div>
-        <div className={`project-item__customer ${props.checkBoxState ? 'barbie':''}`}>{props.customer}</div>
-      </div>      
+    <Card className={`project-item ${isChecked ? "barbie" : ""}`}>
+      <div className={`project-item__description ${isChecked ? "barbie" : ""}`}>
+        <h2>{props.projName}</h2>
+        <div className={`project-year ${isChecked ? "barbie" : ""}`}>
+          {props.year}
+        </div>
+        <div className={`project-item__customer ${isChecked ? "barbie" : ""}`}>
+          {props.customer}
+        </div>
+        <label>
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={changeStateHandler}
+          />
+          Barbie style
+        </label>
+      </div>
     </Card>
   );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,25 +6,11 @@ import Card from "../UI/Card";
 import "./Projects.css";
 
 function Projects(props) {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const changeStateHandler = (event) => {
-    setIsChecked(!isChecked);return;
-  };
-  //console.log('Is "My Value" checked?' + isChecked);
   return (
     <div className="projects">
-      <Card className={`project-item ${isChecked ? 'barbie':''}`}>
-        <div className={`project-item__description ${isChecked ? 'barbie':''}`}>
+      <Card className="project-item">
+        <div className="project-item__description">
           <h2>List of my Projects</h2>
-          <label>
-            <input
-              type="checkbox"
-              checked={isChecked}
-              onChange={changeStateHandler}
-            />
-            Barbie style
-          </label>
         </div>
       </Card>
 
@@ -32,25 +18,21 @@ function Projects(props) {
         projName={props.items[0].projName}
         customer={props.items[0].customer}
         year={props.items[0].year}
-        checkBoxState={isChecked}
       />
       <ProjectItem
         projName={props.items[1].projName}
         customer={props.items[1].customer}
         year={props.items[1].year}
-        checkBoxState={isChecked}
       />
       <ProjectItem
         projName={props.items[2].projName}
         customer={props.items[2].customer}
         year={props.items[2].year}
-        checkBoxState={isChecked}
       />
       <ProjectItem
         projName={props.items[3].projName}
         customer={props.items[3].customer}
         year={props.items[3].year}
-        checkBoxState={isChecked}
       />
     </div>
   );
