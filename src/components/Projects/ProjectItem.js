efd1bc2ad404c0ca9ccbd88013100d43bf11b253
OlgaChayetskaya <Ochayetskaya@ibagroup.eu>
2022-10-07 15:57:14 +0200

    Task4

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { BsPencil } from "react-icons/bs";
+import { FiSave } from "react-icons/fi";
+import { GiCancel } from "react-icons/gi";
 
 import Card from "../UI/Card";
 
@@ -6,30 +9,92 @@ import "./ProjectItem.css";
 
 function ProjectItem(props) {
   const [isChecked, setIsChecked] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
-  const changeStateHandler = (event) => {
+  const [currProjName, setCurrProjName] = useState(props.projName);
+  const [updProjName, setUpdProjName] = useState(currProjName);
+
+  const [currProjDesc, setCurrProjDesc] = useState(props.projDesc);
+  const [updProjDesc, setUpdProjDesc] = useState(currProjDesc);
+
+  const changeStateHandler = () => {
     setIsChecked(!isChecked);
-    return;
   };
+  const changePensilHandler = () => {
+    setEditMode(!editMode);
+  };
+
+  const changeCancelHandler = () => {
+    setEditMode(!editMode);
+    setIsChecked(false);
+    setUpdProjName(currProjName);
+    setUpdProjDesc(currProjDesc);
+  };
+  const changeSaveHandler = () => {
+    setEditMode(!editMode);
+    setIsChecked(false);
+    setCurrProjName(updProjName);
+    setCurrProjDesc(updProjDesc);
+  };
+
+  const changeProjNameHandler = (event) => {
+    setUpdProjName(event.target.value);
+  };
+  const changeProjDescHandler = (event) => {
+    setUpdProjDesc(event.target.value);
+  };
+
   return (
-    <Card className={`project-item ${isChecked ? "barbie" : ""}`}>
-      <div className={`project-item__description ${isChecked ? "barbie" : ""}`}>
-        <h2>{props.projName}</h2>
-        <div className={`project-year ${isChecked ? "barbie" : ""}`}>
-          {props.year}
+    <Card className={`project-item ${isChecked && !editMode ? "barbie" : ""}`}>
+      <Card className="project-item_header">
+        <div
+          className={`project-item__description ${
+            isChecked && !editMode ? "barbie" : ""
+          }`}
+        >
+          <div
+            className={`project-item__customer ${
+              isChecked && !editMode ? "barbie" : ""
+            }`}
+          >
+            {props.customer}
+          </div>
+          {!editMode && <h2>{currProjName} 'Orig'</h2>}
+          {editMode && (
+            <input type="text" value={updProjName} onChange={changeProjNameHandler} />
+          )}
+          <div
+            className={`project-year ${isChecked && !editMode ? "barbie" : ""}`}
+          >
+            {props.year}
+          </div>
+          {!editMode && (
+            <div>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={isChecked}
+                  onChange={changeStateHandler}
+                />
+                Barbie style
+              </label>
+              <BsPencil color="white" onClick={changePensilHandler} />
+            </div>
+          )}
+          {editMode && (
+            <div>
+              <FiSave color="white" onClick={changeSaveHandler} />
+              <GiCancel color="white" onClick={changeCancelHandler} />
+            </div>
+          )}
         </div>
-        <div className={`project-item__customer ${isChecked ? "barbie" : ""}`}>
-          {props.customer}
-        </div>
-        <label>
-          <input
-            type="checkbox"
-            checked={isChecked}
-            onChange={changeStateHandler}
-          />
-          Barbie style
-        </label>
-      </div>
+      </Card>
+      <Card className="project-item_body">
+        {!editMode && <p>{currProjDesc}</p>}
+        {editMode && (
+          <input type="text" value={updProjDesc} onChange={changeProjDescHandler} />
+        )}
+      </Card>
     </Card>
   );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,38 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ProjectItem from "./ProjectItem";
-import Card from "../UI/Card";
+//import Card from "../UI/Card";
 
 import "./Projects.css";
 
 function Projects(props) {
   return (
     <div className="projects">
-      <Card className="project-item">
+      {/* <Card className="project-item">
         <div className="project-item__description">
           <h2>List of my Projects</h2>
         </div>
-      </Card>
+      </Card> */}
 
       <ProjectItem
         projName={props.items[0].projName}
         customer={props.items[0].customer}
         year={props.items[0].year}
+        projDesc={props.items[0].projDesc}
       />
       <ProjectItem
         projName={props.items[1].projName}
         customer={props.items[1].customer}
         year={props.items[1].year}
+        projDesc={props.items[1].projDesc}
+
       />
       <ProjectItem
         projName={props.items[2].projName}
         customer={props.items[2].customer}
         year={props.items[2].year}
+        projDesc={props.items[2].projDesc}
       />
       <ProjectItem
         projName={props.items[3].projName}
         customer={props.items[3].customer}
         year={props.items[3].year}
+        projDesc={props.items[3].projDesc}
       />
     </div>
   );
